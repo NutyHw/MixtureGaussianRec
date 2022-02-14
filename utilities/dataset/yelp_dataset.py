428fc22fa0d49f646c17184184adf75e40c79d91
Nattaphum Saito <nuty@Nattaphums-MacBook-Pro.local>
2022-02-14 19:22:38 +0700

    change yelp dataset

--- utilities/dataset/yelp_dataset.py
+++ utilities/dataset/yelp_dataset.py
@@ -1,67 +1,138 @@
-import os
+import random
+from collections import defaultdict
+import numpy as np
+import scipy.io
 import torch
 from torch.utils.data import Dataset
-from filelock import FileLock
+
+random.seed(7)
 
 class YelpDataset( Dataset ):
-    def __init__( self, r : int ):
-        dataset_dir = '../../process_datasets/yelp2018/'
-        self.relation_dir = os.path.join( dataset_dir, 'relation_mat' )
-        self.train_adj_mat = torch.load( os.path.join( dataset_dir, 'train_adj_mat.pt' ) )
+    def __init__( self ):
+        self.num_samples = 5
+        self.UB, self.BCat, self.BCity = self.load_dataset()
+        self.BCat, self.BCity = self.preprocess_relation( self.BCat ), self.preprocess_relation( self.BCity )
+        self.n_users, self.n_items = self.UB.shape[0], self.UB.shape[1]
 
-        self.n_users = self.train_adj_mat.shape[0]
-        self.n_items = self.train_adj_mat.shape[1]
-
-        if r == -1:
-            self.relation_dataset = { 'train_mask' : torch.ones( ( self.n_users, self.n_items ) ) }
-        else:
-            self.load_dataset( r )
+        pos_train_interact, pos_val_interact, pos_test_interact = self.train_test_val_split( self.UB )
+        self.filter_cold_start( pos_train_interact, pos_val_interact, pos_test_interact )
+        self.val_mask, self.val_score, self.test_mask, self.test_score = self.create_mask()
         self.sampling()
 
     def __len__( self ):
-        return self.pos_train.shape[0]
+        return self.n_users * self.num_samples
 
     def __getitem__( self, idx ):
-        return self.pos_train[ idx ], self.neg_train[ idx ]
+        return self.train_pos_idx[ idx ], self.train_neg_idx[ idx ]
 
     def get_val( self ):
-        return self.relation_dataset[ 'val_interact'  ]
+        return self.val_mask, self.val_score
 
     def get_test( self ):
-        return self.relation_dataset[ 'test_interact' ]
+        return self.test_mask, self.test_score
 
-    def get_reg_mat( self ):
-        return self.interact_mapper
+    def preprocess_relation( self, relation  ):
+        val, indices = relation.data, np.vstack((relation.row, relation.col))
+        shape = relation.shape
+        i = torch.LongTensor(indices)
+        v = torch.FloatTensor( val )
 
-    def load_dataset( self, r ):
-        relation_dataset = dict()
+        return torch.sparse.FloatTensor(i, v, torch.Size(shape)).to_dense()
 
-        relation_dataset = torch.load( os.path.join( self.relation_dir, str(r), 'relation_dataset.pt' ) )
-        entity_mapper = torch.load( os.path.join( self.relation_dir, str(r), 'entity_mapper.pt' ) )
-        interact_mapper = torch.load( os.path.join( self.relation_dir, str(r), 'interact_mapper.pt' ) )
+    def get_reg_mat( self, relation : str ):
+        if relation == 'BCat':
+            return self.BCat
+        elif relation == 'BCity':
+            return self.BCity
 
-        self.relation_dataset = relation_dataset
-        self.entity_mapper = entity_mapper
-        self.interact_mapper = interact_mapper
+    def load_dataset( self ):
+        mat = scipy.io.loadmat( './process_datasets/yelp.mat' )
+        UB, UU, UCom, BCat, BCity = (x.tocoo() for x in list(mat['relation'][0]))
+
+        return UB, BCat, BCity
+
+    def create_pytorch_interact( self, user_id, all_items ):
+        user = torch.full( ( 1, len( all_items ) ), user_id, dtype=torch.long )
+        all_items = torch.tensor( all_items, dtype=torch.long )
+
+        return torch.vstack( ( user, all_items ) ).T
+
+    def train_test_val_split( self, UB ):
+        user_biz = defaultdict(list)
+
+        for u, b in zip(UB.row, UB.col):
+            user_biz[u].append(b)
+
+        pos_train_interact = torch.zeros( ( 0, 2 ), dtype=torch.long )
+        pos_val_interact = torch.zeros( ( 0, 2  ), dtype=torch.long )
+        pos_test_interact = torch.zeros( ( 0, 2 ), dtype=torch.long )
+
+        for u in user_biz:
+            if len(user_biz[u]) >= 5:
+                val = int( len(user_biz[u]) * 0.6 )
+                test = int( len(user_biz[u]) * 0.8 )
+
+                random.shuffle(user_biz[u])
+
+                pos_train_interact = torch.vstack( ( pos_train_interact, self.create_pytorch_interact( u, user_biz[u][:val] ) ) )
+                pos_val_interact = torch.vstack( ( pos_val_interact, self.create_pytorch_interact( u, user_biz[u][val:test] ) ) )
+                pos_test_interact = torch.vstack( ( pos_test_interact, self.create_pytorch_interact( u, user_biz[u][test:] ) ) )
+
+        return pos_train_interact, pos_val_interact, pos_test_interact
+
+    def apply_mask_to_interact( self, user_mask, item_mask, interact ):
+        adj_mat = torch.zeros( ( self.n_users, self.n_items ) )
+        adj_mat[ interact[:,0], interact[:,1] ] = 1
+
+        return adj_mat[ user_mask][ :, item_mask ].nonzero()
+
+    def filter_cold_start( self, pos_train_interact, pos_val_interact, pos_test_interact ):
+        train_adj_mat = torch.zeros( ( self.n_users, self.n_items ) )
+        train_adj_mat[ pos_train_interact[:,0], pos_train_interact[:,1] ] = 1
+
+        user_mask = torch.sum( train_adj_mat, dim=-1 ) > 0
+        train_adj_mat = train_adj_mat[ user_mask ]
+        item_mask = torch.sum( train_adj_mat, dim=0 ) > 0
+        train_adj_mat = train_adj_mat[ :, item_mask ]
+
+        self.train_adj_mat = train_adj_mat
+        self.pos_train_interact = train_adj_mat.nonzero()
+        self.pos_val_interact = self.apply_mask_to_interact( user_mask, item_mask, pos_val_interact )
+        self.pos_test_interact = self.apply_mask_to_interact( user_mask, item_mask, pos_test_interact )
+
+        self.n_users, self.n_items = train_adj_mat.shape[0], train_adj_mat.shape[1]
+
+    def create_mask( self ):
+        val_mask = torch.ones( ( self.n_users, self.n_items ) )
+        test_mask = torch.ones( ( self.n_users, self.n_items ) )
+
+        val_scores = torch.zeros( ( self.n_users, self.n_items ) )
+        test_scores = torch.zeros( ( self.n_users, self.n_items ) )
+
+        val_mask[ self.pos_train_interact[:,0], self.pos_train_interact[:,1] ] = 0
+        val_mask[ self.pos_test_interact[:,0], self.pos_test_interact[:,1] ] = 0
+
+        test_mask[ self.pos_train_interact[:,0], self.pos_train_interact[:,1] ] = 0
+        test_mask[ self.pos_val_interact[:,0], self.pos_val_interact[:,1] ] = 0
+
+        val_scores[ self.pos_val_interact[:,0], self.pos_val_interact[:,1] ] = 1
+        test_scores[ self.pos_test_interact[:,0], self.pos_test_interact[:,1] ] = 1
+
+        return val_mask > 0, val_scores, test_mask > 0, test_scores
 
     def sampling( self ):
-        train_mask = self.relation_dataset[ 'train_mask' ]
+        neg_adj_mat = 1 - self.train_adj_mat
+        num_samples = torch.sum( self.train_adj_mat, dim=-1 ).tolist()
 
-        relation_adj_mat = self.train_adj_mat * train_mask
-        relation_neg_adj_mat = ( 1 - self.train_adj_mat ) * train_mask
+        pos_item_idx = torch.multinomial( self.train_adj_mat, num_samples=self.num_samples, replacement=True ).reshape( -1, 1 )
+        neg_item_idx = torch.multinomial( neg_adj_mat, num_samples=self.num_samples ).reshape( -1, 1 )
 
-        user_mask = torch.sum( relation_adj_mat, dim=-1 ) > 0
+        user_idx = torch.arange( self.n_users ).reshape( -1, 1 ).tile( 1, self.num_samples ).reshape( -1, 1 )
 
-        num_samples = torch.sum( relation_adj_mat, dim=-1 ).tolist()
-        neg_train = torch.zeros( ( 0, 2 ), dtype=torch.long )
+        self.train_pos_idx = torch.hstack( ( user_idx, pos_item_idx ) )
+        self.train_neg_idx = torch.hstack( ( user_idx, neg_item_idx ) )
 
-        for uid in torch.arange( self.train_adj_mat.shape[0] )[ user_mask ].tolist():
-            neg_items = torch.multinomial( relation_neg_adj_mat[ uid ], num_samples=int( num_samples[ uid ] ) ).reshape( -1, 1 )
-            user_idx = torch.full( ( int( num_samples[ uid ] ), 1 ), uid, dtype=torch.long )
-            neg_train = torch.vstack( ( neg_train, torch.hstack( ( user_idx, neg_items ) ) ) )
-
-        self.pos_train, self.neg_train = relation_adj_mat.nonzero(), neg_train
 
 if __name__ == '__main__':
-    dataset = YelpDataset( -1 )
-    print( dataset[256] )
+    dataset = YelpDataset()
+    print( dataset[1999] )
