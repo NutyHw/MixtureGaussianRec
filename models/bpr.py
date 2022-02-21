6e4f4650ad4b6611e60696e6243c17393d542fc8
Nattaphum Saito <nuty@Nattaphums-MacBook-Pro.local>
2022-02-21 13:40:37 +0700

    bpr model

--- models/bpr.py
+++ models/bpr.py
@@ -7,10 +7,16 @@ class BPR(nn.Module):
         self.W = nn.Parameter(torch.normal( 0, 1, ( user_size, dim ) ))
         self.H = nn.Parameter(torch.normal( 0, 1, ( item_size, dim ) ) )
 
-    def forward(self, user_idx : torch.Tensor, item_idx : torch.Tensor ):
+    def forward(self, user_idx : torch.Tensor, item_idx : torch.Tensor, is_test=False ):
+        if is_test:
+            return torch.matmul( self.W, self.H.T )
+
         user_embed = self.W[ user_idx ]
         item_embed = self.H[ item_idx ]
 
-        y = torch.sum( self.W * self.H, dim=-1 )
+        y = torch.sum( user_embed * item_embed, dim=-1 )
 
         return y
+
+if __name__ == '__main__':
+    model = BPR( 10, 10, 16 )
