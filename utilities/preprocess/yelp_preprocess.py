7ca31fcd06391eab4a8a7a6560193e23b245e7af
Nattaphum Saito <nuty@Nattaphums-MacBook-Pro.local>
2022-03-16 17:49:30 +0700

    yelp jacard sim calculation

--- utilities/preprocess/yelp_preprocess.py
+++ utilities/preprocess/yelp_preprocess.py
@@ -14,12 +14,10 @@ def compute_jacard_sim( attribute_tensor : torch.Tensor ):
     '''
 
     jacard_sim = torch.zeros( ( attribute_tensor.shape[0], attribute_tensor.shape[0] ) )
-    for i in range( attribute_tensor.shape[0] - 1 ):
-        for j in range( i + 1, attribute_tensor.shape[0] ):
-            intersect = torch.sum( torch.logical_and( attribute_tensor[i] > 0, attribute_tensor[j] > 0 ) )
-            union = torch.sum( torch.logical_or( attribute_tensor[i] > 0, attribute_tensor[j] > 0 ) )
-            jacard_sim[ i, j ] = intersect / union
-            jacard_sim[ j, i ] = intersect / union
+    for i in range( attribute_tensor.shape[0] ):
+        intersect = torch.sum( attribute_tensor[i].unsqueeze( dim=0 ) * attribute_tensor[ i + 1 : ], dim=-1 )
+        union = torch.sum( ( attribute_tensor[i].unsqueeze( dim=0 ) + attribute_tensor[ i + 1 : ] ) > 0, dim=-1 )
+        jacard_sim[ i, i + 1 : ] = intersect / union
 
     return jacard_sim
 
@@ -29,9 +27,11 @@ if __name__ == '__main__':
     metapath_jacard_sim = dict()
 
     for metapath in metapaths.keys():
+        print( f'start metapath : { metapath }' )
         jacard_sim = compute_jacard_sim( metapaths[ metapath ] )
         metapath_jacard_sim[ metapath ] = jacard_sim
 
+    print( f'start interaction')
     interaction_jacard_sim = {
         'user_user' : compute_jacard_sim( dataset['train_adj_mat'] ),
         'item_item' : compute_jacard_sim( dataset['train_adj_mat'].T )
