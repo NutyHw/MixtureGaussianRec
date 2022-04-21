5bf5c12d05251a57b506d4e3418f5e54a5263881
Nattaphum Saito <nuty@Nattaphums-MacBook-Pro.local>
2022-04-21 16:59:47 +0700

    negative sampling dataset

--- utilities/dataset/yelp_dataset.py
+++ utilities/dataset/yelp_dataset.py
@@ -9,7 +9,8 @@ import numpy as np
 from torch.utils.data import Dataset
 
 class YelpDataset( Dataset ):
-    def __init__( self, process_dataset ):
+    def __init__( self, process_dataset, k ):
+        self.k = k
         with open( os.path.join( process_dataset, 'dataset.npz' ), 'rb' ) as f:
             arr = np.load( f )
             self.ub = torch.from_numpy( arr['ub'] )
@@ -26,13 +27,16 @@ class YelpDataset( Dataset ):
         self.item_input = torch.hstack( ( item_one_hot, self.item_attribute ) ).to( torch.float )  
 
         #self.filter_coldstart_item()
-        #self.samples()
+        self.samples()
 
     def __len__( self ):
-        return self.n_users
+        return self.pos_interact.shape[0]
 
     def __getitem__( self, idx ):
-        return self.user_input[ idx ], self.item_input, self.train_adj_mat[ idx ]
+        negative_idx = torch.randint( self.n_items, ( self.k,  ) )
+        return self.user_input[ self.pos_interact[ idx ][0] ], \
+            self.item_input[ self.pos_interact[ idx ][1] ], \
+            self.item_input[ negative_idx ]
 
     def get_item_attribute( self ):
         return self.item_attribute
@@ -53,22 +57,9 @@ class YelpDataset( Dataset ):
         self.item_input = self.item_input[ :, item_mask ]
 
     def samples( self ):
-        user_count = torch.sum( self.train_adj_mat, dim=-1 ).to( torch.int ).tolist()
-        neg_adj = 1 - ( self.train_adj_mat > 0 ).to( torch.float )
-        neg_adj = neg_adj / torch.sum( neg_adj, dim=-1 ).reshape( -1, 1 )
-
-        neg_interact = torch.zeros( ( 0, 2 ), dtype=torch.long )
-
-        for idx, size in enumerate( user_count ):
-            chosen_items = torch.tensor( np.random.choice( self.n_items, size, p=neg_adj[idx].numpy() ) )
-            users = torch.full( chosen_items.shape, idx )
-            interact = torch.vstack( ( users, chosen_items ) ).T
-            neg_interact = torch.vstack( ( neg_interact, interact ) )
-
         self.pos_interact = self.train_adj_mat.nonzero()
-        self.neg_interact = neg_interact
 
 if __name__ == '__main__':
-    dataset = YelpDataset( './yelp_dataset/train_ratio_0.8/' )
+    dataset = YelpDataset( './yelp_dataset/train_ratio_0.8/', 5 )
     user_input, item_input, train_adj = dataset[0]
     print( user_input.shape, item_input.shape, train_adj.shape )
