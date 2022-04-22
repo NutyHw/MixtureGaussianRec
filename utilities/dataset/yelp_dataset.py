7ea07831bc15ab90145c6d6e0cb4c67b75af5017
Nattaphum Saito <nutsaitoh@gmail.com>
2022-04-23 01:24:43 +0700

    fix conflict

--- utilities/dataset/yelp_dataset.py
+++ utilities/dataset/yelp_dataset.py
@@ -10,24 +10,8 @@ import numpy as np
 from torch.utils.data import Dataset
 
 class YelpDataset( Dataset ):
-<<<<<<< HEAD
     def __init__( self, process_dataset, i, attribute, alpha ):
         self.dataset = torch.load( os.path.join( process_dataset, f'yelp_dataset_{i}.pt' ) )
-=======
-    def __init__( self, process_dataset ):
-        with open( os.path.join( process_dataset, 'dataset.npz' ), 'rb' ) as f:
-            arr = np.load( f )
-            self.ub = torch.from_numpy( arr['ub'] ).to( torch.float )
-            self.item_attribute = torch.from_numpy( arr['bcat'] ).to( torch.float )
-            self.train_mask = torch.from_numpy( arr['train_mask'] ) > 0
-            self.val_mask = torch.from_numpy( arr['val_mask'] ) > 0
-            self.test_mask = torch.from_numpy( arr['test_mask'] ) > 0
-
-        self.train_adj_mat = self.train_mask * self.ub
-        self.filter_no_interact_users()
-        self.n_users, self.n_items = self.train_adj_mat.shape
-        self.n_features = self.item_attribute.shape[1]
->>>>>>> 3dbb4fee7510a9fa5989aa83a2cb2be3654d6409
 
         with open( os.path.join( process_dataset, 'attribute.npy' ), 'rb' ) as f:
             arr = np.load( f )
@@ -72,12 +56,5 @@ class YelpDataset( Dataset ):
         return self.attribute[ idx ]
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     dataset = YelpDataset( './yelp_dataset/', 0, 'BCat', 40 )
     print( dataset[0].type() )
-=======
-    dataset = YelpDataset( './yelp_dataset/train_ratio_0.8/', 20 )
-    user_input, item_input, train_adj = dataset[0]
-    print( dataset.get_item_attribute().shape )
-    print( user_input.shape, item_input.shape, train_adj.shape )
->>>>>>> 3dbb4fee7510a9fa5989aa83a2cb2be3654d6409
