da3aad51974a83973a68f921d07928f4028359b8
Nattaphum Saito <nutsaitoh@gmail.com>
2022-04-24 22:36:33 +0700

    function name change

--- utilities/dataset/dataloader.py
+++ utilities/dataset/dataloader.py
@@ -4,12 +4,8 @@ from torch.utils.data import DataLoader, TensorDataset
 import pytorch_lightning as pl
 
 class Scheduler( pl.Callback ):
-    def on_train_epoch_start( self, trainer, pl_module ):
-        if ( pl_module.current_epoch + 1 ) % 10 == 0:
-            self.mode = not self.mode
-
     def on_train_epoch_end( self, trainer, pl_module ):
-        pl_module.dataset.samples()
+        pl_module.dataset.create_interact()
 
 if __name__ == '__main__':
     dataloader = GeneralDataLoader( 0, 512 )
